7b46410c24d49af4604e37251f94d4791cf0913e
Ingar Abrahamsen <ingar@abrahams1.com>
2016-08-29 23:36:31 +0200

    feat: run db migration on startup

--- src/main/scala/no/javazone/poll/AggregatorMain.scala
+++ src/main/scala/no/javazone/poll/AggregatorMain.scala
@@ -1,9 +1,14 @@
 package no.javazone.poll
 
+import no.javazone.poll.storage.Migration
+
 object AggregatorMain extends App {
 
   println("Starting app")
   private val config: ServerConfiguration = DefaultConfig()
+
+  Migration.runMigration(config.db)
+
   private val fetcher: MqttFetcher = new MqttFetcher(
     config.mqtt.url,
     config.mqtt.port)
--- src/main/scala/no/javazone/poll/ServerConfiguration.scala
+++ src/main/scala/no/javazone/poll/ServerConfiguration.scala
@@ -1,13 +1,24 @@
 package no.javazone.poll
 
-import scala.util.Properties
+import scala.util.Properties._
 
 case class MqttConfig(
     url: String,
     port: Int)
 
+case class DatabaseConfig(
+    driver: String = "org.postgresql.Driver",
+    host: String = "localhost",
+    port: Int = 5432,
+    database: String = "devnull",
+    username: String = "devnull",
+    password: String = "devnull") {
+  val connectionUrl = s"jdbc:postgresql://$host:$port/$database"
+}
+
 case class ServerConfiguration(
-    mqtt: MqttConfig)
+    mqtt: MqttConfig,
+    db: DatabaseConfig )
 
 object DefaultConfig {
 
@@ -15,13 +26,24 @@ object DefaultConfig {
     val mqttUrl: String = propsOrEnv("mqtt.url").get
     val mqttPort: Int = propsOrEnv("mqtt.port").map(_.toInt).getOrElse(1883)
 
-    ServerConfiguration(MqttConfig(mqttUrl, mqttPort))
+
+    ServerConfiguration(
+      MqttConfig(mqttUrl, mqttPort),
+      DatabaseConfigEnv())
   }
 
-  private def propsOrEnv(key: String): Option[String] = {
-    Properties.propOrNone(key).orElse(
-      Properties.envOrNone(key)
+  private def propsOrEnv(key: String): Option[String] =
+    propOrNone(key).orElse(envOrNone(key))
+
+}
+
+object DatabaseConfigEnv {
+
+  def apply(): DatabaseConfig = {
+    DatabaseConfig(
+      database = propOrElse("dbName", envOrElse("DB_NAME", "pollzone")),
+      username = propOrElse("dbUsername", envOrElse("DB_USERNAME", "pollzone")),
+      password = propOrElse("dbPassword", envOrElse("DB_PASSWORD", "pollzone"))
     )
   }
-
 }
