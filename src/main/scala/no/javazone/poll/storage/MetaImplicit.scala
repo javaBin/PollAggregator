d02689e3de32207b8cdd913644ce6710c2f2cc0f
Ingar Abrahamsen <ingar@abrahams1.com>
2016-08-30 01:50:37 +0200

    feat: add mac addr composite

--- src/main/scala/no/javazone/poll/storage/MetaImplicit.scala
+++ src/main/scala/no/javazone/poll/storage/MetaImplicit.scala
@@ -3,12 +3,23 @@ package no.javazone.poll.storage
 import java.time.{LocalDateTime, ZoneOffset}
 
 import doobie.util.meta.Meta
+import doobie.util.meta.Meta._
+import org.postgresql.util.PGobject
+
+import scalaz.Scalaz._
 
 trait MetaImplicit {
 
-  implicit val LocalDateTimeMeta: Meta[LocalDateTime] =
+  implicit val LocalDateTimeType: Meta[LocalDateTime] =
     Meta[java.sql.Timestamp].nxmap(
       ts => LocalDateTime.ofEpochSecond(ts.getTime, 0, ZoneOffset.UTC),
       dt => new java.sql.Timestamp(dt.toEpochSecond(ZoneOffset.UTC))
     )
+
+  implicit val MacAddrType = Meta.other[PGobject]("macaddr")
+      .xmap[String](
+    a => a.getValue,
+    b => new PGobject <| (_.setType("macaddr")) <| (_.setValue(b))
+  )
+
 }
