bce47d69e5606e2e53ef914fb09d8c094a4b0355
Ingar Abrahamsen <ingar@abrahams1.com>
2016-08-30 17:31:54 +0200

    feat: execute the messages async

--- src/main/scala/no/javazone/poll/MqttFetcher.scala
+++ src/main/scala/no/javazone/poll/MqttFetcher.scala
@@ -3,25 +3,28 @@ package no.javazone.poll
 import no.javazone.poll.storage.StorageService
 import org.eclipse.paho.client.mqttv3.{IMqttDeliveryToken, MqttCallback, MqttClient, MqttMessage}
 
+import scala.concurrent.duration.{DurationLong, FiniteDuration}
+import scala.language.postfixOps
 import scalaz.\/
 
 class MqttFetcher(mqtt: MqttConfig, storage: StorageService) {
+  private val retryDur: Seq[FiniteDuration] = Seq(100 millis, 200 millis, 500 millis)
+
   private val client: MqttClient = new MqttClient(s"tcp://${mqtt.url}:${mqtt.port}", "PollAgg")
 
   client.setCallback(new MqttCallback {
 
-    override def deliveryComplete(token: IMqttDeliveryToken): Unit = {}
 
+    override def deliveryComplete(token: IMqttDeliveryToken): Unit = {}
     override def messageArrived(topic: String, message: MqttMessage): Unit = {
       val msg = Message(topic, message)
-      println(s"received msg: $msg")
 
-      def logErrors: (\/[Throwable, Unit]) => Unit = {
+      def logErrors: (\/[Throwable, Any]) => Unit = {
         r => r.swap.toOption.foreach(_.printStackTrace())
       }
       msg match {
-        case o @ OnlineMessage(_,_,_) => storage.onlineEvent(o).runAsync(logErrors)
-        case v @ VoteMessage(_, _, _) => storage.voteEvent(v).run
+        case o @ OnlineMessage(_,_,_) => storage.onlineEvent(o).retry(retryDur).runAsync(logErrors)
+        case v @ VoteMessage(_, _, _) => storage.voteEvent(v).retry(retryDur).runAsync(logErrors)
         case u => println("unhandled msg: $u")
       }
     }
