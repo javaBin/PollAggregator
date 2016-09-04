0dbee980c574a180f0087e8920773a80596c8d72
Ingar Abrahamsen <ingar@abrahams1.com>
2016-09-04 22:46:26 +0200

    feat: print unhandled mqtt messages

--- src/main/scala/no/javazone/poll/MqttFetcher.scala
+++ src/main/scala/no/javazone/poll/MqttFetcher.scala
@@ -25,7 +25,7 @@ class MqttFetcher(mqtt: MqttConfig, storage: StorageService) {
       msg match {
         case o @ OnlineMessage(_,_,_) => storage.onlineEvent(o).retry(retryDur).runAsync(logErrors)
         case v @ VoteMessage(_, _, _) => storage.voteEvent(v).retry(retryDur).runAsync(logErrors)
-        case u => println("unhandled msg: $u")
+        case u => println(s"unhandled msg: $u")
       }
     }
 
